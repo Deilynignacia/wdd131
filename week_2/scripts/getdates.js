411e67b18bbfade717afd81216eccf76ca60ff0f
Deilyn Zamudio <deilynzamudio@gmail.com>
2025-07-22 02:55:20 -0400

    place

---
+++ week_2/scripts/getdates.js
@@ -0,0 +1,10 @@
+// 2025
+const currentYearSpan = document.getElementById('currentyear');
+const today = new Date();
+const currentYear = today.getFullYear();
+currentYearSpan.textContent = currentYear;
+
+// Last Modified
+const lastModifiedParagraph = document.getElementById('lastModified');
+const lastModifiedDate = document.lastModified;
+lastModifiedParagraph.textContent = `Last Modified: ${lastModifiedDate}`;
