978de5752e3795c7ece1d4d7815fccddd7a30693
Deilyn Zamudio <deilynzamudio@gmail.com>
2025-08-03 21:50:02 -0400

    form html

---
+++ scripts/form.js
@@ -0,0 +1,47 @@
+// Menu
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});
+
+// Footer
+const currentYearSpan = document.getElementById('currentyear');
+const today = new Date();
+const currentYear = today.getFullYear();
+currentYearSpan.textContent = currentYear;
+
+const lastModifiedParagraph = document.getElementById('lastModified');
+const lastModifiedDate = document.lastModified; 
+lastModifiedParagraph.textContent = `Last Modified: ${lastModifiedDate}`;
+
+// Product Array
+const products = [
+  {
+    id: "fc-1888",
+    name: "flux capacitor",
+    averagerating: 4.5
+  },
+  {
+    id: "fc-2050",
+    name: "power laces",
+    averagerating: 4.7
+  },
+  {
+    id: "fs-1987",
+    name: "time circuits",
+    averagerating: 3.5
+  },
+  {
+    id: "ac-2000",
+    name: "low voltage reactor",
+    averagerating: 3.9
+  },
+  {
+    id: "jj-1969",
+    name: "warp equalizer",
+    averagerating: 5.0
+  }
+];
